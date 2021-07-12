07ea7a836ba596797d799a8eca7b5885be0458dd
ekene-ezedi <ezedi.eo.ekene@gmail.com>
2021-07-12 12:28:16 +0100

    add content-type to request options

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { User } from '../shared/types/user';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment'
@@ -9,11 +9,16 @@ import { environment } from '../../environments/environment'
 })
 export class AuthService {
   private apiUrl:string = environment.apiUrl;
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+    })
+  }
 
   constructor(private http:HttpClient) { }
 
   //register new user
   signup(user:User):Observable<any>{
-    return this.http.post(`${this.apiUrl}/users`,user)
+    return this.http.post(`${this.apiUrl}/users`,user,this.httpOptions)
   }
 }
